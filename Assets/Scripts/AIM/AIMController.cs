c045d8eff3455e4717afaf12b9ccfe845db3d69b
Aashiq Parker <Aashiq Parker>
2016-08-11 15:04:04 +0200

    Added basic turning functionality. Separated the method into the AIMController script.

--- Assets/Scripts/AIM/AIMController.cs
+++ Assets/Scripts/AIM/AIMController.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AIMController : MonoBehaviour {
+public class AIMController : SimpleHeuristicController {
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	protected override void Update () {
+        base.Update();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "IntersectionManager") //TODO: Store types in a config file
+        {
+            Debug.Log("Entered IM");
+            driving = false;
+            StartCoroutine("Turn");
+        }
+    }
+    //TODO: Fix interpolation
+    IEnumerator Turn() 
+    {
+        int steps = 5; //Number of points on the Bezier curve
+        int counter = 1;
+
+        while (counter <= steps)
+        {
+            Vector3 toPoint = BZ.GetComponent<BezierCurve>().GetPointAt((float)(steps - counter) / (steps));
+            //Vector3 dir = toPoint - transform.position;
+            // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // cube.transform.position = toPoint;
+            //transform.LookAt(new Vector3(toPoint.x, transform.position.y, toPoint.z));
+            //transform.position += dir * Time.deltaTime * speed;
+            float step = speed * Time.deltaTime;
+            Vector3 newPos = Vector3.MoveTowards(transform.position, new Vector3(toPoint.x, transform.position.y, toPoint.z), step);
+            transform.LookAt(newPos);
+            transform.position = newPos; //transform.position = new Vector3(toPoint.x, transform.position.y, toPoint.z);
+            if (transform.position == new Vector3(toPoint.x, transform.position.y, toPoint.z))
+                ++counter;
+            //Debug.Log(toPoint);
+            yield return null;
+        }
+        driving = true;
+        Debug.Log("Left IM");
+        //transform.position = Vector3.Slerp
+    }
+
 }
--- Assets/Scripts/AIM/SimpleHeuristicController.cs
+++ Assets/Scripts/AIM/SimpleHeuristicController.cs
@@ -4,50 +4,23 @@ using System.Collections;
 public class SimpleHeuristicController : MonoBehaviour {
 
     public GameObject BZ;
-    private bool hasControl;
+    protected bool driving;
     public float speed = 10f;
 	// Use this for initialization
-	void Start () {
-        hasControl = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (hasControl)
-            Move();
-        else Turn();
+	protected virtual void Start () {
+        driving = true;
 	}
 
-    void Move ()
+    // Update is called once per frame
+    protected virtual void Update () {
+        if (driving)
+            Move();
+	}
+
+    protected void Move ()
     {
         transform.position += transform.forward * speed * Time.deltaTime; //Too naive
     }
 
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag == "IntersectionManager") //TODO: Store types in a config file
-        {
-            Debug.Log("Entered IM");
-            hasControl = false;
-            StartCoroutine("Turn");
-        }
-    }
-
-    IEnumerator Turn() //TODO: Separate, move to AIMController
-    {
-        int steps = 30; //Number of points on the Bezier curve 
-        for(int i = 0; i < steps; ++i)
-        {
-            Vector3 toPoint = BZ.GetComponent<BezierCurve>().GetPointAt((float)(steps-1-i) /(steps-1));
-            Vector3 dir = toPoint - transform.position;
-            // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            // cube.transform.position = toPoint;
-            transform.LookAt(toPoint);
-            transform.position += dir * Time.deltaTime * speed;
-            //Debug.Log(toPoint);
-            yield return null;
-        }
-        hasControl = true;
-        //transform.position = Vector3.Slerp
-    }
+   
 }
