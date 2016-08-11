b40921b69de04fa5fe41ec734aca55fb92e1247b
Aashiq Parker <Aashiq Parker>
2016-08-11 15:23:59 +0200

    Added some references to other Lanes in the Lane script. Added a Util script for general methods and other stuff.

--- Assets/Scripts/AIM/Lane.cs
+++ Assets/Scripts/AIM/Lane.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Lane : MonoBehaviour {
+
+    //Lanes the vehicle can turn into, null otherwise
+    public Lane right;
+    public Lane left;
+    public Lane straight;
+
+    //Bezier Curves for the turn trajectory, null otherwise
+    public GameObject leftPath;
+    public GameObject rightPath;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/AIM/Util.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util : MonoBehaviour {
+
+    public static enum Direction {LEFT,
+                                  RIGHT,
+                                  STRAIGHT };
+}
