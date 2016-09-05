198a3b1a147738860db6880a6c445faae7b2a782
Amposter <Amp940703@gmail.com>
2016-09-05 11:25:34 +0200

    Added the functionality to simulate  intersection driving internally. Started putting together the pieces for the IntersectionManager.

--- Assets/Scripts/AIM/AIMController.cs
+++ Assets/Scripts/AIM/AIMController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class AIMController : SimpleHeuristicController {
@@ -13,6 +14,7 @@ public class AIMController : SimpleHeuristicController {
     public Direction[] path;
     public GameObject currLane;
     private int nextDir; //Next direction index in the 'path' list to take
+    private int steps = 5; //Number of bezier points to sample 
 
     // Use this for initialization
     protected override void Start () {
@@ -31,6 +33,7 @@ public class AIMController : SimpleHeuristicController {
         {
             Debug.Log("Entered IM");
             driving = false;
+            SimulatePath();
             StartCoroutine("Turn");
         }
 
@@ -63,23 +66,32 @@ public class AIMController : SimpleHeuristicController {
             Debug.Log("Going Straight");
             yield break;
         }
-        int steps = 5; //Number of points on the Bezier curve
         int counter = 1;
-
+        Debug.Log("Initial Time Actual: " + Time.time);
+        Vector3 toPoint = curve.GetPointAt((float)(steps - counter) / (steps));
+        toPoint.y = transform.position.y;
+        transform.LookAt(toPoint);
         while (counter <= steps)
         {
-            Vector3 toPoint = curve.GetPointAt((float)(steps - counter) / (steps));
-            //Vector3 dir = toPoint - transform.position;
-            // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+           // Vector3 dira = toPoint - transform.position;
+            // GameObject cube = Gam4eObject.CreatePrimitive(PrimitiveType.Cube);
             // cube.transform.position = toPoint;
             //transform.LookAt(new Vector3(toPoint.x, transform.position.y, toPoint.z));
-            //transform.position += dir * Time.deltaTime * speed;
-            float step = speed * Time.deltaTime;
-            Vector3 newPos = Vector3.MoveTowards(transform.position, new Vector3(toPoint.x, transform.position.y, toPoint.z), step);
-            transform.LookAt(newPos);
-            transform.position = newPos; //transform.position = new Vector3(toPoint.x, transform.position.y, toPoint.z);
-            if (transform.position == new Vector3(toPoint.x, transform.position.y, toPoint.z))
+           // transform.position += dira * Time.deltaTime * speed;
+              Vector3 newPos = Vector3.MoveTowards(transform.position, toPoint, speed * Time.deltaTime);
+              transform.LookAt(newPos);
+              transform.position = newPos; //transform.position = new Vector3(toPoint.x, transform.position.y, toPoint.z);*/
+          //  transform.position += transform.forward * speed * Time.deltaTime;
+            if (transform.position == toPoint)
+            {
+               // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                 Debug.Log("CurrTime: " + Time.time + " " + transform.position);
+                //cube.transform.position = new Vector3(toPoint.x, transform.position.y, toPoint.z);
                 ++counter;
+                toPoint = curve.GetPointAt((float)(steps - counter) / (steps));
+                toPoint.y = transform.position.y;
+                transform.LookAt(toPoint);
+            }
             //Debug.Log(toPoint);
             yield return null;
         }
@@ -87,4 +99,63 @@ public class AIMController : SimpleHeuristicController {
         Debug.Log("Left IM");
     }
 
+    KeyValuePair<float, Vector3>[] SimulatePath()
+    {
+        KeyValuePair<float, Vector3>[] output = new KeyValuePair<float, Vector3>[steps];
+        Direction dir = path[nextDir];
+        BezierCurve curve = null; //TODO: Fix unassigned ref error (not by making this null)
+
+        if (dir == Direction.RIGHT)
+        {
+            curve = currLane.GetComponent<Lane>().rightPath.GetComponent<BezierCurve>();
+        }
+        else if (dir == Direction.LEFT)
+        {
+            curve = currLane.GetComponent<Lane>().leftPath.GetComponent<BezierCurve>();
+        }
+        else if (dir == Direction.STRAIGHT) //Take this straight checkout, dont call method if straight
+        {
+            driving = true;
+            Debug.Log("Going Straight");
+            return null;
+        }
+
+        float initialTime = Time.time;
+        Debug.Log("Initial time simulation: " + initialTime);
+        float distance = 0;
+        Vector3 curr = transform.position; // curve.GetPointAt(0.8f);
+        Vector3 next;
+        int res = 15; //Number of points used to approximate length of curve segment
+        for (int s = 1; s < steps+1; ++s)
+        {
+
+            float segment = (1.0f - (s/ (float)steps)); // s -> (s-1)
+            Vector3 point = curve.GetPointAt(segment); 
+            point.y = transform.position.y;
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = point;
+            /*  for (int i = 1; i < res + 1; ++i)
+              {
+                  next = curve.GetPointAt(segment - ((i / (float)res) * (1/(float)steps)));
+                  next.y = curr.y;
+                  distance += (next - curr).magnitude;
+                  curr = next;
+              }*/
+            next = curve.GetPointAt(segment);
+            next.y = curr.y;
+            distance += (next - curr).magnitude;
+            curr = next;
+
+            float elapsedTime = distance / speed;
+         //   Debug.Log(elapsedTime + initialTime);
+            output[s - 1] = new KeyValuePair<float, Vector3>(initialTime+elapsedTime, point);
+          //  Debug.Log("Predicted time: " + (Time.time + time));
+        }
+        Debug.Log(output[0].ToString());
+        Debug.Log(output[1].ToString());
+        Debug.Log(output[2].ToString());
+        Debug.Log(output[3].ToString());
+        Debug.Log(output[4].ToString());
+        return output;
+    }
 }
--- Assets/Scripts/AIM/IntersectionManager.cs
+++ Assets/Scripts/AIM/IntersectionManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class IntersectionManager : MonoBehaviour {
 
+    KeyValuePair<float, List<Vector3>> reservations;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        reservations = new KeyValuePair<float, List<Vector3>>;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
