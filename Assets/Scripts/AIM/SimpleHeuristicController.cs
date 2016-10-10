2ae2ab5fc84f17a080754525b846dfa7d8621ae4
Amposter <Amp940703@gmail.com>
2016-10-10 13:27:14 +0200

    Added an update point method to the simple controller.

--- Assets/Scripts/AIM/SimpleHeuristicController.cs
+++ Assets/Scripts/AIM/SimpleHeuristicController.cs
@@ -6,28 +6,30 @@ public class SimpleHeuristicController : VehicleController
 
     //Public variabes
     public float speed = 5f;
+    public bool paused;
 
     //Private variables
-    public bool paused;
     private BezierCurve curve;
     private BezierCurve[] curves;
 
+    private int offset;
+    private Vector3 toPoint;
 
     protected int resolution; //Number of points to sample on each path curve
 
-	// Use this for initialization
-	protected override void Start ()
-	{
-		base.Start();
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
         paused = false;
-        resolution = 20;
-	}
+        resolution = 25;
+    }
 
     // Update is called once per frame
-	protected override void Update ()
+    protected override void Update()
     {
-		base.Update ();
-	}
+        base.Update();
+    }
 
     public void setCurve(BezierCurve curve)
     {
@@ -41,40 +43,75 @@ public class SimpleHeuristicController : VehicleController
 
     protected virtual IEnumerator AutoDrive()
     {
-
-        foreach (BezierCurve currentCurve in curves)
+        foreach (BezierCurve curve in curves)
         {
-            int offset = 1;
-			resolution = 20;
-			Vector3 toPoint = currentCurve.GetPointAt(offset / (float)resolution);
-			toPoint.y = gameObject.transform.position.y;
-			gameObject.transform.transform.LookAt(toPoint);
+            this.curve = curve;
+            offset = 1;
+            toPoint = curve.GetPointAt(offset / (float)resolution);
+            toPoint.y = transform.position.y;
+            transform.LookAt(toPoint);
 
             while (offset <= resolution)
             {
                 while (paused) //Do nothing
                 {
-                    yield return null;
+                    yield return new WaitForFixedUpdate();
                 }
-				Vector3 newPos = Vector3.MoveTowards(gameObject.transform.position, toPoint, speed * Time.deltaTime);
-				gameObject.transform.LookAt(newPos);
-				gameObject.transform.position = newPos;
+
+                Vector3 newPos = Vector3.MoveTowards(transform.position, toPoint, speed * Time.deltaTime);
+                transform.LookAt(newPos);
+                transform.position = newPos;
                 if (transform.position == toPoint)
                 {
                     ++offset;
-					toPoint = currentCurve.GetPointAt((float)offset / resolution);
-					toPoint.y = gameObject.transform.position.y;
-					gameObject.transform.LookAt(toPoint);
+                    toPoint = curve.GetPointAt((float)offset / resolution);
+                    toPoint.y = transform.position.y;
+                    transform.LookAt(toPoint);
                 }
-				yield return new WaitForFixedUpdate();
+                yield return new WaitForFixedUpdate();
             }
         }
     }
-    protected virtual IEnumerator Drive ()
+
+    protected void AutoDriveFrame()
     {
-		
-        int offset = 1;
-        Vector3 toPoint = curve.GetPointAt(offset /(float)resolution);
+
+    }
+
+    //Update the offset along the BezierCurve to be the point closest to object's current position
+    protected void UpdateNextPoint()
+    {
+        int temp = offset;
+        Vector3 point = curve.GetPointAt(temp / (float)resolution);
+        point.y = transform.position.y;
+        float minDist = (point - transform.position).magnitude;
+        float dist;
+
+        while (++temp <= resolution)
+        {
+            point = curve.GetPointAt(temp / (float)resolution);
+            point.y = transform.position.y;
+            dist = (point - transform.position).magnitude;
+            if (dist > minDist)
+                break;
+            else
+                minDist = dist;
+        }
+
+        //Debug.Log(offset + " new " + (temp - 1));
+        //  if (temp < resolution) //Move to the next point if you can
+        //     offset = temp;
+        //  else
+        offset = temp - 1;
+
+        toPoint = curve.GetPointAt((float)offset / resolution);
+        toPoint.y = transform.position.y;
+    }
+
+    protected virtual IEnumerator Drive()
+    {
+        offset = 1;
+        Vector3 toPoint = curve.GetPointAt(offset / (float)resolution);
         toPoint.y = transform.position.y;
         transform.LookAt(toPoint);
 
@@ -82,19 +119,19 @@ public class SimpleHeuristicController : VehicleController
         {
             while (paused) //Do nothing
             {
-				yield return null;
+                yield return null;
             }
             Vector3 newPos = Vector3.MoveTowards(transform.position, toPoint, speed * Time.deltaTime);
             transform.LookAt(newPos);
-            transform.position = newPos; 
+            transform.position = newPos;
             if (transform.position == toPoint)
-            {         
+            {
                 ++offset;
-                toPoint = curve.GetPointAt((float)offset/resolution);
+                toPoint = curve.GetPointAt((float)offset / resolution);
                 toPoint.y = transform.position.y;
                 transform.LookAt(toPoint);
             }
-			yield return new WaitForFixedUpdate();
+            yield return null;
         }
     }
 
